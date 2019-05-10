6a99630b39f6ca1955d278f68003fcfced3f0e29
zakiatus safara <zakiatussafara@gmail.com>
2019-05-10 13:01:27 +0700

    ...

--- application/controllers/ikan_user.php
+++ application/controllers/ikan_user.php
@@ -3,7 +3,7 @@ class ikan_user extends CI_Controller
 {
     public function ikan_u(){
       $data['main_view'] = 'ikan';
-      $this->load->view('layout/mainUser',$data);
+      $this->load->view('ikan',$data);
     }
   }
  ?>
