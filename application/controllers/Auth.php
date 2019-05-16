3d04f53c573652307653db8b401d6b518b695613
Vinni Elfia Rosa <vinnielfiar@gmail.com>
2019-05-16 16:50:09 +0700

    menambahkan controller auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,7 +11,7 @@ class Auth extends CI_Controller
 
     public function index()
     {
-        $this->load->view('home');
+        $this->load->view('Userhome');
     }
 
     public function Userlogin()
